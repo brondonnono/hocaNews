76da6cc85cba552ad966b92f2cf8a1efa537d77d
Brondon Nono <brondonnono3@gmail.com>
2023-07-03 19:50:59 +0100

    generation of news page and update of app device mode and icons 03/07/2023 19:50

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'news',
+        loadChildren: () => import('../news/news.module').then(m => m.NewsPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
