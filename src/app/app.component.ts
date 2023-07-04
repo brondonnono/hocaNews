a8ecdb058b922f9716126a2f1890df87b68a385e
Brondon Nono <brondonnono3@gmail.com>
2023-07-04 20:38:42 +0100

    starting to add the sliding-menu 04/07/2023 20:38

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  public appPages: any = [];
+  
   constructor() {}
+
+  public setMenuItem(): any[] {
+    return this.appPages = [
+      { title: 'Programme', url: '/app/tabs/tab1', icon: 'calendar' },
+      { title: 'history', url: '/history', icon: 'book' },
+      { title: 'Actualités', url: '/app/tabs/news', icon: 'news' },
+      { title: 'Mes états', url: '/app/tabs/tab2', icon: 'bar-chart' },
+      { title: 'Statut de l\'association', url: '/status', icon: 'info' },
+      { title: 'A propos', url: '/about', icon: 'infos' },
+    ];
+  }
 }
