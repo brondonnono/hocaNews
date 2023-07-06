71e6d9cbb150c2b7a509714efac3314628694ba5
Brondon Nono <brondonnono3@gmail.com>
2023-07-06 09:38:41 +0100

    integration of components in visualize page 06/07/2023 09:38

--- src/app/visualize/visualize.module.ts
+++ src/app/visualize/visualize.module.ts
@@ -7,12 +7,22 @@ import { IonicModule } from '@ionic/angular';
 import { VisualizePageRoutingModule } from './visualize-routing.module';
 
 import { VisualizePage } from './visualize.page';
+import { BankComponentModule } from '../components/bank/bank.module';
+import { FondComponentModule } from '../components/fond/fond.module';
+import { OrderComponentModule } from '../components/order/order.module';
+import { PresenceComponentModule } from '../components/presence/presence.module';
+import { StateComponentModule } from '../components/state/state.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    BankComponentModule,
+    OrderComponentModule,
+    StateComponentModule,
+    PresenceComponentModule,
+    FondComponentModule,
     VisualizePageRoutingModule
   ],
   declarations: [VisualizePage]
