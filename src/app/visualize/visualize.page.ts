9abe19502c4e178890d4b2cf271a65f99b639475
Brondon Nono <brondonnono3@gmail.com>
2023-07-06 08:31:49 +0100

    updating visualize page to get the reference of the clicked submenu and show the correct title 06/07/2023 08:31

--- src/app/visualize/visualize.page.ts
+++ src/app/visualize/visualize.page.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-visualize',
   templateUrl: './visualize.page.html',
   styleUrls: ['./visualize.page.scss'],
 })
-export class VisualizePage implements OnInit {
+export class VisualizePage {
 
-  constructor() { }
+  defaultHref = '/app/tabs/tab2';
+  data: any;
+  contentRef = this.route.snapshot.paramMap.get('contentRef');
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ionViewWillEnter() {
+    this.contentRef = this.route.snapshot.paramMap.get('contentRef');
+    if (this.contentRef != null) this.data = this.getData(this.contentRef);
+    else this.router.navigate(['/tab1']);
   }
 
+  getData(contentRef: string) { }
+
+  getTitle() {
+    let res = '';
+    switch (this.contentRef) {
+      case 'order':
+        res = 'Odre de cotisation';
+        break;
+
+      case 'fond':
+        res = 'Fond';
+        break;
+
+      case 'presence':
+        res = 'Présence';
+        break;
+
+      case 'bank':
+        res = 'Banque';
+        break;
+
+      case 'state':
+        res = 'Etats cotisations';
+        break;
+
+      default:
+        break;
+    }
+    return res;
+  }
 }
