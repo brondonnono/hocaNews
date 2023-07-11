0aeb80fbe232c25e56f72089c468fd4a855b9b7f
Brondon Nono <brondonnono3@gmail.com>
2023-07-11 16:39:42 +0100

    updating tabs page calling utility service to get the value of all new Data tabs 11/07/2023 16:39

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UtilityService } from '../services/utility.service';
 
 @Component({
   selector: 'app-tabs',
@@ -12,6 +13,59 @@ export class TabsPage {
   isNewStates: boolean = false;
   isNewProfil: boolean = false;
 
-  constructor() {}
+  newProgramNumber: number = 0;
+  newActusNumber: number = 0;
+  newProfilNumber: number = 0;
+  newStatesNumber: number = 0;
+
+  constructor(private utilityService: UtilityService) {
+    for (const type in ["Program", "Profil", "Actus", "States"]) {
+      let value = this.utilityService.checkIsExistNewData(type);
+      if (value != 0) {
+        switch (type) {
+          case "Program":
+            this.isNewProgram = true;
+            this.newProgramNumber = value;
+            break;
+
+          case "Profil":
+            this.isNewProfil = true;
+            this.newProfilNumber = value;
+            break;
+
+          case "Actus":
+            this.isNewActus = true;
+            this.newActusNumber = value;
+            break;
+
+          case "States":
+            this.isNewStates = true;
+            this.newStatesNumber = value;
+            break;
+        }
+      } else {
+        switch (type) {
+          case "Program":
+            this.isNewProgram = false;
+            break;
+
+          case "Profil":
+            this.isNewProfil = false;
+            break;
+
+          case "Actus":
+            this.isNewActus = false;
+            break;
+
+          case "States":
+            this.isNewStates = false;
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+  }
 
 }
