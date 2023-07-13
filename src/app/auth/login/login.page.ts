5f0d02a9f2862d0162888a9f79c81162c0be42ea
Brondon Nono <brondonnono3@gmail.com>
2023-07-13 19:10:40 +0100

    adding login page style 13/07/2023 19:10

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public credentials = { email: '', password: '' };
+  public eyeMode = 'open';
+  public submitted = false;
+  public isActiveToggleTextPassword = true;
+  public errorMessages = { username: '', password: '' };
+  public isLoading = false;
+  private isLogin = true;
 
-  ngOnInit() {
+  constructor(
+    public menu: MenuController
+  ) { }
+
+  ngOnInit() { }
+
+  public getType() {
+    return this.isActiveToggleTextPassword ? 'password' : 'text';
+  }
+
+  public ionViewWillEnter(): void {
+    this.menu.enable(false);
+  }
+
+  public ionViewWillLeave(): void {
+    this.menu.enable(true);
+  }
+
+  public showPassword(): void {
+    switch (this.isLogin) {
+      case true:
+        if (this.eyeMode === 'open') {
+          this.isActiveToggleTextPassword = false;
+          this.eyeMode = 'close';
+        } else {
+          this.isActiveToggleTextPassword = true;
+          this.eyeMode = 'open';
+        }
+        break;
+      
+      case false:
+        if (this.eyeMode === 'open') {
+          this.isActiveToggleTextPassword = false;
+          this.eyeMode = 'close';
+        } else {
+          this.isActiveToggleTextPassword = true;
+          this.eyeMode = 'open';
+        }
+        break;
+      
+      default:
+        break;
+    }
   }
 
 }
