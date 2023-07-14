4271139e78506d6a2760a804834d4c422f16b9af
Brondon Nono <brondonnono3@gmail.com>
2023-07-14 23:09:11 +0100

    many update done and starting to create icon and splashscreen for plateforms 14/07/2023 23:08

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCA16REGVoHYrEUM-mX7tfT3eFpaK3ITDM",
+    authDomain: "oriontask-f6f96.firebaseapp.com",
+    databaseURL: "https://oriontask-f6f96-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "oriontask-f6f96",
+    storageBucket: "oriontask-f6f96.appspot.com",
+    messagingSenderId: "246331312522",
+    appId: "1:246331312522:web:f26771821e31498c31e073",
+    measurementId: "G-7H01JF4FYW"
+  }
 };
